19c124cf1d59e4791c68d7b7b39267d39a32f126
Jared Lewis <jlewis8493@gmail.com>
2022-01-24 15:44:21 -0500

    BankEncapsulation build successful

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jaredsAccount = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            jaredsAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is now {jaredsAccount.GetBalance()}");
+        }
+    }
+}
